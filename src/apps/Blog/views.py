946aa7c11c1e371e94a24737bf97382f0ca8e19f
melkor <oleg.bakiev.jrs@gmail.com>
2023-09-13 18:05:06 +0300

    blog logic commit

--- src/apps/Blog/views.py
+++ src/apps/Blog/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from .models import ArticleModel
 
 
@@ -7,3 +7,9 @@ class BlogView(ListView):
     model = ArticleModel
     template_name = 'blog.html'
     context_object_name = 'blog'
+
+
+class ArticleView(DetailView):
+    model = ArticleModel
+    template_name = 'detail.html'
+    context_object_name = 'detail'
