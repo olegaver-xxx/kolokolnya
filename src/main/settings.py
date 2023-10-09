d162418b63a7f7939f554d1dc437b79fe520ac1a
paulwinex <paulwinex@gmail.com>
2023-10-09 23:03:20 +0300

    added jazzmin settings

--- src/main/settings.py
+++ src/main/settings.py
@@ -125,3 +125,126 @@ CKEDITOR_BASEPATH = "/static/ckeditor/ckeditor/"
 #         'toolbar': 'Basic',
 #     },
 # }
+
+JAZZMIN_SETTINGS = {
+    # title of the window
+    "site_title": os.getenv('SITE_TITLE') or "Колокольня",
+
+    # Title on the brand, and the login screen (19 chars max)
+    "site_header": os.getenv('SITE_TITLE') or "Колокольня",
+
+    'site_brand': os.getenv('SITE_TITLE') or 'Колокольня',
+
+    # square logo to use for your site, must be present in static files, used for favicon and brand on top left
+    "site_logo": os.getenv('SITE_LOGO') or '/images/logo.png',
+    "site_icon": None,
+
+    # Welcome text on the login screen
+    "welcome_sign": "Welcome to the Kololo",
+
+    # Copyright on the footer
+    "copyright": "Oleg",
+
+    # The model admin to search from the search bar, search bar omitted if excluded
+    # "search_model": "core.SGUser",
+
+    # Field name on user model that contains avatar image
+    "user_avatar": None,
+
+    ############
+    # Top Menu #
+    ############
+
+    # Links to put along the top menu
+    "topmenu_links": [
+        # {"name": "Index", 'url': 'index'},
+
+        # Url that gets reversed (Permissions can be added)
+        {"name": "View Site",  "url": "home", "permissions": []},
+
+    ],
+
+    #############
+    # User Menu #
+    #############
+
+    # Additional links to include in the user menu on the top right ("app" url type is not allowed)
+    # "usermenu_links": [
+    #     {"name": "Support", "url": "https://github.com/farridav/django-jazzmin/issues", "new_window": True},
+    #     # {"model": "auth.user"}
+    # ],
+
+    #############
+    # Side Menu #
+    #############
+
+    # Whether to display the side menu
+    "show_sidebar": True,
+
+    # Whether to aut expand the menu
+    "navigation_expanded": True,
+
+    # Hide these apps when generating side menu e.g (auth)
+    "hide_apps": [],
+
+    # Hide these models when generating side menu (e.g auth.user)
+    "hide_models": [],
+
+    # List of apps (and/or models) to base side menu ordering off of (does not need to contain all apps/models)
+    "order_with_respect_to": ['auth', 'users'],
+
+    # Custom links to append to app groups, keyed on app name
+    # "custom_links": {
+    #     "shortcuts": [{
+    #         "name": "Preferences2",
+    #         "url": "index",
+    #         "icon": "fas fa-comments",
+    #         # "permissions": ["books.view_book"]
+    #     }]
+    # },
+
+    # Custom icons for side menu apps/models See https://fontawesome.com/icons?d=gallery&m=free
+    # for a list of icon classes
+    "icons": {
+        "users.UserModel": "fas fa-user",
+        # "sguser.Settings": "fas fa-user-cog",
+        "auth.Group": "fas fa-users-cog",
+        # "auth.Group": "fas fa-users",
+        "auth.Permission": "fas fa-user-lock",
+
+    },
+    # Icons that are used when one is not manually specified
+    "default_icon_parents": "fas fa-chevron-circle-right",
+    "default_icon_children": "fas fa-circle",
+
+    #################
+    # Related Modal #
+    #################
+    # Use modals instead of popups
+    "related_modal_active": False,
+
+    #############
+    # UI Tweaks #
+    #############
+    # Relative paths to custom CSS/JS scripts (must be present in static files)
+    "custom_css": "css/custom_admin.css",
+    "custom_js": None,
+    # Whether to show the UI customizer on the sidebar
+    "show_ui_builder": False,
+
+    ###############
+    # Change view #
+    ###############
+    # Render out the change view as a single form, or in tabs, current options are
+    # - single
+    # - horizontal_tabs (default)
+    # - vertical_tabs
+    # - collapsible
+    # - carousel
+    "changeform_format": "vertical_tabs",
+    # override change forms on a per modeladmin basis
+    # "changeform_format_overrides": {"auth.user": "collapsible", "auth.group": "vertical_tabs",},
+    # Add a language dropdown into the admin
+    "language_chooser": False,
+
+}
