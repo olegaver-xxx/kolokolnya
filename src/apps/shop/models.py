cf97612f1045977423154f10b76b0dae36120fa6
paulwinex <paulwinex@gmail.com>
2023-10-15 22:11:51 +0300

    fix m2m field

--- src/apps/shop/models.py
+++ src/apps/shop/models.py
@@ -9,7 +9,7 @@ class Product(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField(max_length=1000)
     price = models.DecimalField(max_digits=10, decimal_places=2)
-    tags = models.ManyToManyField('Tag', blank=True, null=True)
+    tags = models.ManyToManyField('Tag', blank=True)
 
     def __str__(self):
         return self.name
