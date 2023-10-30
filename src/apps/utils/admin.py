679bddd59907459c7dfee5b26ab8a288922ed1f3
paulwinex <paulwinex@gmail.com>
2023-10-30 21:44:15 +0300

    updated text block admin class

--- src/apps/utils/admin.py
+++ src/apps/utils/admin.py
@@ -1,7 +1,16 @@
-from django.contrib import admin
+import re
 
+from django.contrib import admin
+from django.utils.html import strip_tags
+from django.utils.text import Truncator
 from .models import TextBlockModel
 
-admin.site.register(TextBlockModel)
 
+@admin.register(TextBlockModel)
+class TextBlockAdmin(admin.ModelAdmin):
+    list_display = 'name', 'text_preview'
+
+    def text_preview(self, obj):
+        text = Truncator(re.sub(r"&\w+;", " ", strip_tags(obj.text))).words(10, truncate='...')
+        return text
 
