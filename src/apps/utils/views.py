8f1da5c891332b687173ef27b7cc5b12381d71da
paulwinex <paulwinex@gmail.com>
2023-11-12 18:32:36 +0300

    fix prefs page access

--- src/apps/utils/views.py
+++ src/apps/utils/views.py
@@ -1,11 +1,17 @@
 from django.contrib import admin
 from django.views.generic import TemplateView, ListView
 from apps.utils.services import get_prefs_form, set_prefs
+from django.http import HttpResponseForbidden
 from .models import MainGallery
 
 
 class PreferencesView(TemplateView):
     template_name = 'prefs.html'
+
+    def dispatch(self, request, *args, **kwargs):
+        if not request.user.is_superuser:
+            return HttpResponseForbidden()
+        return super().dispatch(request, *args, **kwargs)
 
     def get_context_data(self, **kwargs):
         ctx = super(PreferencesView, self).get_context_data(**kwargs)
