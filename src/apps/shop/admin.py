d72a273f060507ee469f8ce6550f9b126055f754
paulwinex <paulwinex@gmail.com>
2023-12-11 22:48:38 +0300

    added json widget

--- src/apps/shop/admin.py
+++ src/apps/shop/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from .models import Product, ProductImage, Cart, CartProduct, Tag, Order, Record
+from django.db import models
+from django_json_widget.widgets import JSONEditorWidget
 
 
 class ProductImageInline(admin.TabularInline):
@@ -21,7 +23,9 @@ class CartAdmin(admin.ModelAdmin):
     inlines = [CartProductInline]
     fields = 'user', 'active', 'status', 'computed_sum', 'payment_id', 'order_details'
     list_display = 'user', 'status', 'computed_sum', 'pk'
-
+    formfield_overrides = {
+        models.JSONField: {'widget': JSONEditorWidget},
+    }
 
 admin.site.register(Tag)
 # admin.site.register(Order)
